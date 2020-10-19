927afc6fef102f1ba87962d376e9d95324bc15c0
TheodorAifadis <theodor.aifadis@skola.botkyrka.se>
2020-10-19 15:38:22 +0200

    solve-10

---
+++ 10/script.js
@@ -0,0 +1,5 @@
+function divisibleByThree(arr){
+    return arr % 3 === 0
+}
+
+console.log(divisibleByThree(15))
---
+++ 9/script.js
@@ -0,0 +1,7 @@
+function toCaps(arr) {
+    return arr.toUpperCase()
+}
+
+let names = ['theo', 'anton', 'victor']
+
+console.log(toCaps(names))
